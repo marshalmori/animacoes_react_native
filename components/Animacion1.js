7d1524916c0229c591301d294a5bd1572f9e5bdb
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-05-29 11:10:42 -0300

    Trabalhar com timing - opacity

--- components/Animacion1.js
+++ components/Animacion1.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, Animated} from 'react-native';
 
 const Animacion1 = () => {
-  return <Text>Animacion1</Text>;
+  const [animacion] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(animacion, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
+  return (
+    <Animated.View
+      style={{
+        opacity: animacion,
+      }}>
+      <Text style={styles.texto}>Animacion1</Text>
+    </Animated.View>
+  );
 };
+
+const styles = StyleSheet.create({
+  texto: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
 
 export default Animacion1;
